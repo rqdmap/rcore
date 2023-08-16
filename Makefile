dc3646c9009697077c02470e50fadced811cef22
rqdmap <55649208+rqdmap@users.noreply.github.com>
2023-08-16 17:37:39 +0800

    更新Ch1结构

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ default:
 	qemu-system-riscv64 \
 		-machine virt \
 		-nographic \
-		-bios ../bootloader/rustsbi-qemu.bin \
+		-bios ./bootloader/rustsbi-qemu.bin \
 		-device loader,file=target/riscv64gc-unknown-none-elf/release/os.bin,addr=0x80200000 \
 
 
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 qemu-system-riscv64 \
     -machine virt \
     -nographic \
-    -bios ../bootloader/rustsbi-qemu.bin \
+    -bios ./bootloader/rustsbi-qemu.bin \
     -device loader,file=target/riscv64gc-unknown-none-elf/release/os.bin,addr=0x80200000 \
     -s -S
 
